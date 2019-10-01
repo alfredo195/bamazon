e75441931013278023992ee29ad4cafbeffa124c
BlackAce <alfredo.gon195@gmail.com>
2019-09-30 23:36:59 -0700

    got the lay out down have to tie some lose ends

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -23,9 +23,6 @@ connection.connect(function(err) {
   });
 
   function start() {
-    connection.query("SELECT * FROM products", function(err, res) {
-      if (err) throw err;
-      console.table(res)
       inquirer
       .prompt({
           name: "buySomething",
@@ -40,10 +37,62 @@ connection.connect(function(err) {
               connection.end()
           }
       })
-    });
-  }
+    }
 
   function openStore(){
-      
+      connection.query("SELECT * FROM products", function(err, res){
+        if(err) throw err;
+
+        inquirer
+        .prompt([
+          {
+            name: "choice",
+            type: "rawlist",
+            choices: function() {
+              var choiceArray = [];
+              for (var i = 0; i < res.length; i++){
+                choiceArray.push(res[i].product_name);
+              }
+              return choiceArray;
+            },
+            message: " What would you like to buy?"
+          },
+          {
+            name: "item",
+            type: "input",
+            message: "how many would you like to buy?"
+          }
+        ])
+        .then(function(answer){
+          var pickedItem
+          for (var i =0; i < res.length; i++){
+            if(results[i].product_name === answer.choice){
+              pickedItem = res[i];
+            }
+          }
+
+          if(pickedItem.item > parseInt(answer.stock_quantity)){
+            connection.query(
+              "UPDATE products SET ? WHERE?",
+              [
+                {
+                  stock_quantity: answer.item
+                }
+              ],
+              function(err){
+                if(err) throw err;
+                console.log("your purchase was successful!");
+                console.table(res)
+                start();
+              }
+            )
+          }
+          else{
+            console.log("not enough in stock try again")
+            start()
+          }
+        })
+      })
+
   }
   
